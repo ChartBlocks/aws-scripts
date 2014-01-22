76526530217c6de083e4d369c1e4dcf121b140d3
arron <arron@copiadigital.co.uk>
2014-01-22 13:58:39 +0000

    Exclude and dry run options

--- ec2-attached-volumes-snapshot.php
+++ ec2-attached-volumes-snapshot.php
@@ -13,7 +13,9 @@ $opt = new Getopt(array(
     'key|O-s' => 'AWS access key',
     'secret|W-s' => 'AWS secret key',
     'hostname|h-s' => 'Hostname to backup',
-    'region|r-s' => 'Region of host'
+    'region|r-s' => 'Region of host',
+    'exclude|i-s' => 'Exclude volumes/devices, comma delimited',
+    'dry-run|d' => 'Dry run, do not delete anything',
         ));
 $opt->parse();
 
@@ -22,6 +24,8 @@ try {
     $secret = empty($opt->secret) ? getenv('AWS_SECRET_KEY') : $opt->secret;
     $region = empty($opt->region) ? getenv('AWS_REGION') : $opt->region;
     $hostname = empty($opt->hostname) ? gethostname() : $opt->hostname;
+    $exclusions = empty($opt->exclude) ? array() : array_map('trim', explode(',', $opt->exclude));
+    $dryRun = (bool) $opt->{'dry-run'};
 
     $console->writeLine("Creating snapshots of $hostname");
 
@@ -48,38 +52,44 @@ try {
         $volumeId = $attachment['VolumeId'];
         $device = $attachment['Device'];
 
-        array_push($toBackup, array(
-            'volumeId' => $volumeId,
-            'device' => $device,
-        ));
+        if (in_array($volumeId, $exclusions) || in_array($device, $exclusions)) {
+            $console->writeLine("Excluding $volumeId on $device", Color::YELLOW);
+        } else {
+            array_push($toBackup, array(
+                'volumeId' => $volumeId,
+                'device' => $device,
+            ));
 
-        $console->writeLine("Found $volumeId on $device", Color::GRAY);
+            $console->writeLine("Found $volumeId on $device", Color::GRAY);
+        }
     }
 
-    $console->writeLine("Starting snapshots");
-    $snapshotIds = array();
-    foreach ($toBackup as $backup) {
-        $volumeId = $backup['volumeId'];
-        $name = $instanceName . ' ' . $backup['device'];
+    if (!$dryRun) {
+        $console->writeLine("Starting snapshots");
+        $snapshotIds = array();
+        foreach ($toBackup as $backup) {
+            $volumeId = $backup['volumeId'];
+            $name = $instanceName . ' ' . $backup['device'];
 
-        $result = $client->createSnapshot(array(
-            'VolumeId' => $volumeId,
-            'Description' => $name
-        ));
+            $result = $client->createSnapshot(array(
+                'VolumeId' => $volumeId,
+                'Description' => $name
+            ));
 
-        $snapshotId = $result['SnapshotId'];
-        array_push($snapshotIds, $snapshotId);
+            $snapshotId = $result['SnapshotId'];
+            array_push($snapshotIds, $snapshotId);
 
-        $console->writeLine("Saving $volumeId on $device to $snapshotId", Color::GRAY);
+            $console->writeLine("Saving $volumeId on $device to $snapshotId", Color::GRAY);
+        }
+
+        $snapshots = $client->describeSnapshots(array(
+                    'SnapshotIds' => $snapshotIds
+                ))->get('Snapshots');
+
+        $console->writeLine('Waiting for snapshots to complete');
+        $client->waitUntilSnapshotCompleted($snapshots);
+        $console->writeLine('All snapshots complete', Color::GREEN);
     }
-
-    $snapshots = $client->describeSnapshots(array(
-        'SnapshotIds' => $snapshotIds
-    ))->get('Snapshots');
-
-    $console->writeLine('Waiting for snapshots to complete');
-    $client->waitUntilSnapshotCompleted($snapshots);
-    $console->writeLine('All snapshots complete', Color::GREEN);
 } Catch (Exception $e) {
     $console->writeLine($e->getMessage(), Color::RED);
     exit(1);
