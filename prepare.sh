901e7facc601af8dbf9b164a7c72d1cc6371545b
arron <arron@copiadigital.co.uk>
2014-01-22 12:19:26 +0000

    Prepare script

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /etc/aws-security
+
+EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed -e 's:\([0-9][0-9]*\)[a-z]*\$:\\1:'`"
+
+export AWS_REGION=${EC2_REGION}
